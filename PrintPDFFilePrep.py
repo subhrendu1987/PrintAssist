e24035f4bf6997a97d4917c0e9bd721003ab2b4e
subhrendu@iitg.ernet.in <subhrendu@idrbt.ac.in>
2024-10-10 09:10:10 +0530

    modified:   PrintPDFFilePrep.py

--- PrintPDFFilePrep.py
+++ PrintPDFFilePrep.py
@@ -1,7 +1,12 @@
 import os
 import argparse
 import sys
-
+from PyPDF2 import PdfReader, PdfWriter
+########################################################################
+def extract_path_and_filename(full_path):
+    directory = os.path.dirname(full_path)
+    filename = os.path.basename(full_path)
+    return directory, filename
 ########################################################################
 def create_folder_for_pdf(pdf_path):
     # Get the directory and the filename without the extension
@@ -16,6 +21,37 @@ def create_folder_for_pdf(pdf_path):
         print(f"Folder '{folder_name}' already exists.")
     return(folder_path)
 ########################################################################
+def split_odd_even_pages(pdf_file, output_file="ALL.pdf",start=0,end=None):
+    path,op_filename=extract_path_and_filename(output_file)
+    try:
+        # Open the PDF file
+        reader = PdfReader(pdf_file)
+        writer = PdfWriter()
+
+        # Get the total number of pages in the PDF
+        num_pages = len(reader.pages)
+        if(output_file==None):
+            return(num_pages)
+        end=num_pages if(end==None) else None
+        # Collect even-numbered pages in reverse order
+        odd_pages= [page for page in range(start,end) if (page) % 2 != 0]
+        # Collect even-numbered pages in reverse order
+        even_pages = [page for page in range(end,start,-1) if (page + 1) % 2 == 0]
+
+        # Add the reversed even pages to the writer
+        for page_num in even_pages:
+            writer.add_page(reader.pages[page_num])
+
+        # Write the even pages to the output PDF file
+        with open(output_file, "wb") as output_pdf:
+            writer.write(output_pdf)
+
+        print(f"Even pages in reverse order saved to '{output_file}'")
+
+    except Exception as e:
+        print(f"Error processing PDF file: {e}")
+########################################################################
+########################################################################
 if __name__ == "__main__":
     # Parse the command-line argument
     parser = argparse.ArgumentParser(description="Prepare Large PDF file for both side printing in a single function printer.")
@@ -25,5 +61,6 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     args.file="Input/Wrox.Professional.Linux.Kernel.Architecture.Oct.2008.pdf"
-    # Call the function to create the folder
-    create_folder_for_pdf(args.file)
+
+    Target_folder=create_folder_for_pdf(args.file)
+    extract_even_pages_in_reverse(args.file,Target_folder+"/ALL.pdf")
